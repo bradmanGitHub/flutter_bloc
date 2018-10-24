18eb26aeb055db0dd5b92bce852b02acb679a6fd
mongkonchai buayai <mongkonchai_kk@hotmail.com>
2018-10-24 13:13:45 +0700

    'kk'

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          new Container(
+            margin: EdgeInsets.only(top: 25.0),
+          ),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@example.com',
+        labelText: 'Email Address',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Password',
+        labelText: 'Password',
+      ),
+      obscureText: true,
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Login'),
+      color: Colors.blue,
+      onPressed: () {},
+    );
+  }
+}
